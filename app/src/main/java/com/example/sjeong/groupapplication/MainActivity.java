7a0a7a25029b057be2130e102f7e8bd4a15456d1
kjh1992 <wer2wer306@gmail.com>
2017-05-03 22:22:56 +0900

    go

--- app/src/main/java/com/example/sjeong/groupapplication/MainActivity.java
+++ app/src/main/java/com/example/sjeong/groupapplication/MainActivity.java
@@ -1,19 +1,58 @@
 package com.example.sjeong.groupapplication;
 
+import android.content.Intent;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
+import android.widget.TextView;
+import android.database.Cursor;
+import android.app.Activity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private String Tag="test MainActive";
 
+    Button bttn_option;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.i(Tag, "success");
-        Log.i(Tag, "success!!");
-        Log.i(Tag, "success!!!");
+
+
+        bttn_option = (Button) findViewById(R.id.bttn_option);
+
+        bttn_option.setOnClickListener(new View.OnClickListener() {
+
+
+
+            public void onClick(View v) {
+
+                Intent intent1 = new Intent(MainActivity.this, OptionActivity.class);
+
+                startActivity(intent1);
+                overridePendingTransition(R.anim.anim_slide_in_left, R.anim.anim_slide_out_right);
+                finish();
+
+
+
+
+            }
+
+        });
+
+
     }
+
+
+
 }
+
+
+
+
+
+
---
+++ app/src/main/java/com/example/sjeong/groupapplication/OptionActivity.java
@@ -0,0 +1,64 @@
+package com.example.sjeong.groupapplication;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import android.content.DialogInterface;
+import android.app.AlertDialog;
+import android.view.View.OnClickListener;
+
+
+
+public class OptionActivity extends AppCompatActivity implements OnClickListener{
+
+    final Context context = this;
+    Button bttn_main, bttn_exm,bttn_mode ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_option);
+
+        bttn_main = (Button) findViewById(R.id.bttn_main);
+        bttn_exm = (Button) findViewById(R.id.bttn_exm);
+        bttn_mode = (Button) findViewById(R.id.bttn_mode);
+
+        bttn_main.setOnClickListener(this);
+        bttn_exm.setOnClickListener(this);
+        bttn_mode.setOnClickListener(this);
+    }
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.bttn_main:
+                Intent intent1 = new Intent(OptionActivity.this,MainActivity.class);
+
+                startActivity(intent1);
+                overridePendingTransition(R.anim.anim_slide_in_left, R.anim.anim_slide_out_right);
+                finish();
+
+                break;
+
+
+            case R.id.bttn_mode:
+
+                Intent intent4 = new Intent(OptionActivity.this,Mode1Activity.class);
+
+                startActivity(intent4);
+                overridePendingTransition(R.anim.anim_slide_in_left, R.anim.anim_slide_out_right);
+                finish();
+
+
+            default:
+                break;
+        }
+    }
+}
+
+
+
+
