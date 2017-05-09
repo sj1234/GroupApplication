06b577a5d7d82172a617282433eeb7918f477b0e
kjh1992 <wer2wer306@gmail.com>
2017-05-09 17:08:47 +0900

    h

--- app/src/main/java/com/example/sjeong/groupapplication/MainActivity.java
+++ app/src/main/java/com/example/sjeong/groupapplication/MainActivity.java
@@ -12,7 +12,7 @@ import android.widget.TextView;
 import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-
+    int z = 0;
     private ImageButton home;
     private ImageButton mode;
     private ImageButton schedule;
